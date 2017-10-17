95dbc0ec7479f2a92b7e8bf98841d54700260835
student201525 <okunev.nikita@yandex.ru>
2017-10-17 18:39:06 +0500

    New2.0

--- Web/menu.php
+++ Web/menu.php
@@ -3,7 +3,7 @@
 class Menu
 {
 	public static $menu = array(
-	1 => array("link" => "Start", "index" => "assets/start.html"),
+	1 => array('link' => 'Start', 'index' => 'assets/start.html'),
 	2 => array('link' => 'About', 'index' => 'assets/about.html'),
 	3 => array('link' => 'Top List', 'index' => 'assets/topList.html')
 	);
@@ -11,9 +11,9 @@ class Menu
 	static public function getItem($var = 0)
 	{
 		echo "<ul class='menu'>";
-		foreach (self::$menu as $item)
+		foreach (self::$menu as $item => $val)
 		{
-				echo "<li><a href='{$item[index]}'> {$item [link]}</a></li>";
+				echo "<li><a href=index.php?page=$item>{$val['link']}</a></li>";
 		}
 	
 
